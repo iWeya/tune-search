bb5cb1ae315d173b79085051aad203e7d383a0c0
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-01-29 03:35:33 -0300

    Criar componente de paginação

---
+++ src/components/PaginationSection.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="level is-mobile">
+		<div class="level-left">
+			<button
+				class="button pagination-previous"
+				:disabled="offset < 10 || disabled"
+
+				@click="previous"
+			>
+				<font-awesome-icon icon="arrow-left" />
+			</button>
+		</div>
+
+		<div class="level-right">
+			<button
+				class="button pagination-previous"
+				:disabled="itemCount < 10 || disabled"
+
+				@click="next"
+			>
+				<font-awesome-icon icon="arrow-right" />
+			</button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+		}
+	},
+
+	methods: {
+		next() {
+			this.updateParams(this.offset + 10)
+		},
+
+		previous() {
+			this.updateParams(this.offset - 10)
+		},
+
+		updateParams(newOffset) {
+			this.$router.replace({
+				name: this.$route.name,
+				params: {
+					offset: newOffset,
+					query: this.$route.params.query
+				}
+			})
+		}
+	},
+
+	name: 'PaginationSection',
+
+	props: ['disabled', 'itemCount', 'offset']
+};
+</script>
