dce5e641da569e4520d99d520bdf5c30e65afc51
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-01-29 03:35:51 -0300

    Criar componente ItemBoxList

---
+++ src/components/ItemBoxList.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="container">
+		<item-box
+			v-for="item in itemList.results"
+			:key="item.trackId"
+
+			:item-data="item"
+		/>
+	</div>
+</template>
+
+<script>
+import ItemBox from './ItemBox'
+
+export default {
+	components: {
+		ItemBox
+	},
+
+	data() {
+		return {
+		}
+	},
+
+	name: 'ItemBoxList',
+
+	props: ['itemList']
+};
+</script>
