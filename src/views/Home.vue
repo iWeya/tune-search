d4cbe7c5d6f4cdcbd58485e2a69b93273871a010
Lucas Vinicius <lucasvinicius211@yahoo.com>
2020-01-29 03:34:50 -0300

    Criar view inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,71 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="hero is-fullheight">
+		<img class="x-decorative-image" alt="" src="../assets/header.min.svg">
+
+		<div class="hero-body">
+			<div class="container">
+				<div class="columns is-centered">
+					<div class="column is-4">
+						<main>
+							<h1 class="title is-spaced x-logo">
+								<img src="../assets/logo-large.min.svg" alt="Logotipo">
+							</h1>
+
+							<form class="box" action="" @submit="searchValue">
+								<div class="field has-addons">
+									<div class="control is-expanded">
+										<input
+											class="input"
+											autocomplete="on"
+											placeholder="Pesquise por álbuns, artistas ou músicas"
+											required
+
+											v-model="query"
+										>
+									</div>
+
+									<div class="control">
+										<button
+											class="button is-primary x-rainbow-background"
+											title="Pesquisar"
+											type="submit"
+										>
+											<span class="icon">
+												<font-awesome-icon icon="search" />
+											</span>
+										</button>
+									</div>
+								</div>
+							</form>
+						</main>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<img class="x-decorative-image" alt="" src="../assets/footer.min.svg">
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+	data() {
+		return {
+			query: ''
+		}
+	},
+
+	methods: {
+		searchValue() {
+			this.$router.push({
+				name: 'search',
+				params: {
+					query: this.query
+				}
+			})
+		}
+	},
+
+	name: 'home'
+};
 </script>
